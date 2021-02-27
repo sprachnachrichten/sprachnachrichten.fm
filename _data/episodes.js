d589e7afe0dc3ed100483d532ba5ccaf883c9824
Arne Bahlo <hallo@arne.me>
2021-02-27 13:59:40 +0100

    This uses the wordpress domain for data
    We need to replace some urls. Once we switch to this we can set the url
    in the wordpress settings and remove them from the json.

--- _data/episodes.js
+++ _data/episodes.js
@@ -1,6 +1,7 @@
 const Cache = require("@11ty/eleventy-cache-assets");
+const settings = require('./settings');
 
-const API_ENDPOINT = 'https://sprachnachrichten.fm/wp-json'
+const API_ENDPOINT = `${settings.wordpressBase}/wp-json`
 
 const CACHE_OPTS = {
 	duration: '1h',
@@ -15,6 +16,18 @@ module.exports = async function() {
 		const episodeId = episodeIndex.results[i].id;
 		let episode = await Cache(`${API_ENDPOINT}/podlove/v1/episodes/${episodeId}`, CACHE_OPTS);
 		episode.link = `/episoden/${episode.number}`
+
+		// Update url to new settings
+		episode.poster = episode.poster.replace(settings.oldWordpressBase, settings.wordpressBase);
+		episode.audio = episode.audio.map(audio => {
+			audio.url = audio.url.replace(settings.oldWordpressBase, settings.wordpressBase);
+			return audio
+		})
+		episode.files = episode.audio.map(file => {
+			file.url = file.url.replace(settings.oldWordpressBase, settings.wordpressBase);
+			return file
+		})
+
 		episodes.push(episode);
 	}
 
