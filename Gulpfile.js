5c54398fddefa3c4005c138e7fb75ea976316906
Jan Früchtl <jan@fruechtl.me>
2021-02-28 11:21:20 +0100

    Add optimizations for profile and cover images

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const cssnano = require('gulp-cssnano');
+const responsive = require('gulp-responsive');
 const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('css', function() {
@@ -17,10 +18,40 @@ gulp.task('js', function() {
 	.pipe(gulp.dest('./_includes/js'));
 });
 
+gulp.task('images', function () {
+  return gulp.src('assets/images/original/**/*.{png,jpg}')
+    .pipe(responsive({
+      '**/*': [{
+        width: 96,
+        rename: { suffix: '-96px' },
+      },{
+        width: 192,
+        rename: { suffix: '-192px' },
+      },{
+        width: 256,
+        rename: { suffix: '-256px' },
+      },{
+        width: 512,
+        rename: { suffix: '-512px' },
+      },{
+        width: 1024,
+        rename: { suffix: '-1024px' },
+      }]
+    },{
+      quality: 80,
+      progressive: true,
+      withMetadata: false,
+      withoutEnlargement: true,
+      errorOnUnusedImage: false,
+      errorOnEnlargement: false
+    }))
+    .pipe(gulp.dest('assets/images/optimized'))
+})
+
 gulp.task("watch", function() {
   gulp.watch('./assets/scss/**/*.scss', gulp.parallel('css'));
   gulp.watch('./assets/js/**/*.js', gulp.parallel('js'));
 });
 
 gulp.task('dev', gulp.parallel('css', 'js'));
-gulp.task('build', gulp.parallel('css', 'js'));
+gulp.task('build', gulp.parallel('css', 'js', 'images'));
