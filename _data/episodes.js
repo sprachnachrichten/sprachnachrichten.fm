9c36158678b3b55bafe948deb5204395693f984f
Arne Bahlo <hey@arne.me>
2022-02-02 11:13:56 +0100

    This adds contributors to the transcripts
    It also sets the cache time to 10min

--- _data/episodes.js
+++ _data/episodes.js
@@ -1,15 +1,22 @@
 const Cache = require("@11ty/eleventy-cache-assets");
 const settings = require('./settings');
+const hosts = require('./hosts');
 
 const API_ENDPOINT = `${settings.wordpressBase}/wp-json`
 
 const CACHE_OPTS = {
-	duration: '1h',
+	duration: '10m',
 	type: 'json'
 }
 
 module.exports = async function () {
 	const episodeIndex = await Cache(`${API_ENDPOINT}/podlove/v1/episodes`, CACHE_OPTS);
+
+	const contributors = hosts.map(contributor => ({
+		id: `${contributor.id}`,
+		name: contributor.name,
+		avatar: `/assets/images/optimized/${contributor.profileImage}-96px.jpg`,
+	}));
 
 	let episodes = [];
 	for (let i = 0; i < episodeIndex.results.length; i++) {
@@ -19,7 +26,7 @@ module.exports = async function () {
 
 		let transcripts = await Cache(`${API_ENDPOINT}/podlove/v1/transcripts/${episodeId}`, CACHE_OPTS);
 		episode.transcripts = transcripts;
-
+		episode.contributors = contributors;
 		episodes.push(episode);
 	}
 
